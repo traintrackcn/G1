be4a090bd4dbe5c9d26e891ac3794b7fc9a00355
traintrackcn <yunfei.tao@2vive.com>
2014-10-14 22:14:01 +0800

    added player & planet sprites

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private int STATE_IDLE = 0;
+	private int STATE_WAIT = 1;
+	private int STATE_WALK = 2;
+	private int STATE_JET = 3;
+	private string keyOfState = "AnimState";
+
+
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Player is created");
+		animator = GetComponent<Animator> ();
+
+		animator.SetInteger(keyOfState, STATE_WAIT);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
