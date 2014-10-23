eec34c220ea9f4717578f405d2a33028891af48c
traintrackcn <traintrackcn@gmail.com>
2014-10-23 21:33:58 +0800

    adjusted dir

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+	CarConnector connector;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Connect (Car obj){
+
+		if (connector) {
+			return;
+		}
+
+		//create a connector instance form prefab
+
+//		var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//		cube.AddComponent(Rigidbody);
+//		Instantiate(prefab, pos, Quaternion.identity);
+
+		connector = new CarConnector ();
+		connector.Bind (this, obj);
+	}
+
+	public void Disconnect (){
+
+		if (connector == null) {
+			return;
+		}
+
+		connector.Unbind(); 
+		connector = null;
+	}
+
+}
