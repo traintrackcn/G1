b41688729793bca08fcaf4886afdb9d8f58a6f3e
traintrackcn <yunfei.tao@2vive.com>
2014-10-23 21:20:23 +0800

    added CarConnector & Planet ops

---
+++ Assets/Scripts/CarConnector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarConnector : MonoBehaviour {
+
+	BoxCollider2D collider2d;
+
+	// Use this for initialization
+	void Start () {
+//		 = new BoxCollider2D ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Bind(Car master, Car slave){
+//		WheelJoint2D joint2d = new WheelJoint2D ();
+//		joint2d.anchor
+	}
+
+	public void Unbind(){
+		
+	}
+}
